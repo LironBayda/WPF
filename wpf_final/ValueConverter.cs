aa0a97e903b835ff381aa3e690c6b6569b22048a
LironBayda <liron.bayda@gmail.com>
2021-05-09 23:50:11 +0300

    first commit

---
+++ wpf_final/ValueConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Data;
+
+namespace wpf_final
+{
+    public class ValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            
+            double num=(double)value;
+            int min;
+            Int32.TryParse(parameter.ToString().Split('_')[0], out min);
+            int max;
+            Int32.TryParse(parameter.ToString().Split('_')[1], out max);
+            int len = max - min;
+
+            if ((num-min)/len*100> 75)
+            {
+                return "EXTRA LARGE";
+            }
+            else if ((num - min) / len * 100 > 50)
+                return "LARGE";
+            else if ((num - min) / len * 100 > 25)
+                return "MEDIUM";
+            else
+                return "SMALL";
+
+
+        }
+
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}   
+
---
+++ wpf_final/Window1.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace wpf_final
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+      
+
+
+        public Window1(double width, double highet, string text)
+        {
+            InitializeComponent();
+            DataContext = this;
+            widthLabel.Content = width;
+            highetLabel.Content = highet;
+            textLabel.Content= text;
+
+        }
+    }
+}
